9e8439a053b05cf430a4e9a2326c821bb8db2270
prophet satin <prophetsatin@gmail.com>
2023-09-24 17:24:58 +0300

    mf

--- helpers_functions/matrix_input_output.py
+++ helpers_functions/matrix_input_output.py
@@ -1,24 +1,18 @@
-def input_matrix(row, col):
+def input_matrix(row_col):
     matrix = []
-    for r in range(row):
-        row = []
-        for c in range(col):
-            element = int(input())
-            row.append(element)
+    for r in range(row_col[0]):
+        row = list(map(int, input().split()))
         matrix.append(row)
     return matrix
 
 
-def print_matrix(matrix, row, col, space=2):
-    for r in range(row):
-        for c in range(col):
+def print_matrix(matrix, row_col, space=2):
+    for r in range(row_col[0]):
+        for c in range(row_col[1]):
             print(str(matrix[r][c]).ljust(space), end=' ')
         print()
 
 
-row, col = int(input()), int(input())
-
-
-matrix = input_matrix(row, col)
-print_matrix(matrix, row, col)
-
+row_col = list(map(int, input().split()))
+matrix = input_matrix(row_col)
+print_matrix(matrix, row_col)
