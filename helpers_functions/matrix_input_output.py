48056d1ad14e24da056cf5b9041a16b64be0e9e1
prophet satin <prophetsatin@gmail.com>
2023-10-01 11:21:52 +0300

    matrix two ways to print

--- helpers_functions/matrix_input_output.py
+++ helpers_functions/matrix_input_output.py
@@ -6,6 +6,14 @@ def print_matrix(matrix, row):
     [print(*matrix[i]) for i in range(row)]
 
 
+# def print_matrix_with_space(matrix, row_col):
+#     for r in range(row_col[0]):
+#         for c in range(row_col[1]):
+#             print(str(matrix[r][c]).ljust(2), end=' ')
+#         print()
+
+
 row, col = [int(i) for i in input().split()]
 matrix = input_matrix(row)
+print()
 print_matrix(matrix, row)
